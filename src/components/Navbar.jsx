b91e59bcd4c757e3064c0b9b0d8d515f8f9b3212
ubeysaab <aboosh.sa262000@gmail.com>
2025-01-11 21:38:23 +0300

    finish navbar components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import { ImSun } from "react-icons/im";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdClose } from "react-icons/md";
@@ -6,23 +6,23 @@ import { BsFillMoonStarsFill } from "react-icons/bs";
 import logo from '../assets/logo.png'
 
 function Navbar() {
-  const [navState,setNavState]=useState(false)
+  const [navState, setNavState] = useState(false)
   return (
     <nav className='navbar'>
-      <div  className='brand'>
-          <img src={logo} alt="" className='border-red' />
-          <div className="toggle-container">
-            <div className="toggle"></div>
-            <div className="mode"></div>
-          </div>
+      <div className='brand'>
+        <img src={logo} alt="" className='border-red' />
       </div>
-      <div className='links-container'>
-      <ul className="links">
-        <li><a href="#">Feature</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Launch</a></li>
-        <li><a href="#">SignUp</a></li>
-      </ul>
+      <div className="toggle-container">
+        <div className="toggle" onClick={() => { setNavState(prv => !prv) }}>{navState ? <MdClose /> : <GiHamburgerMenu />}</div>
+        <div className="mode"><ImSun /></div>
+        <div className={navState ? "links-container--open" : "links-container"}>
+          <ul className="links">
+            <li><a href="#">Feature</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Launch</a></li>
+            <li><a href="#">SignUp</a></li>
+          </ul>
+        </div>
       </div>
 
 
